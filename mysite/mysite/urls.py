0281a60be1691fe43d05b82b8cfac74b7dcd2a4d
Devansh Jain <j.devansh29@gmail.com>
2017-09-20 02:00:24 -0700

    update urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('personal.urls')),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^comments', include('django_comments.urls'))
+    url(r'^blog/', include('blog.urls'))
 
 ]
