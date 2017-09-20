449f907107333db7be7d6a29b97e5a1b24439484
Devansh Jain <j.devansh29@gmail.com>
2017-09-20 01:06:34 -0700

    changing requirements.txt

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,6 +126,10 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES = {'default': dj_database_url.config()}
+
+
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['TEST'] = {'NAME': DATABASES['default']['NAME']}
 
