eeb71f0842a0fee7c6942ab8fe6d50d1fd7e2463
Devansh Jain <j.devansh29@gmail.com>
2017-09-24 17:09:11 -0700

    remove development files

--- mysite/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.views.generic import ListView, DetailView
-from django.contrib import admin
-from . import views
-from blog.models import Post
-urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date"), template_name="blog/blog.html" ))]
